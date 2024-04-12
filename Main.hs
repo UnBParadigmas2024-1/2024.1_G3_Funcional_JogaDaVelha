a0dc19d31e346ac910d52a0cf857d56e8d20e26d
lorenzo7377 <lorenzodavinci@hotmail.com>
2024-04-12 11:47:46 -0300

    Correção na validação

--- Main.hs
+++ Main.hs
@@ -1,4 +1,6 @@
 import Data.List (intercalate)
+import Data.Char (chr, ord)
+
 
 -- Definicao dos tipos de dados
 data Player = White | Black deriving (Eq, Show)
@@ -21,24 +23,27 @@ printBoard board = do
     showPiece (Pawn Black) = "B"
 
 -- Função para validar uma posição no tabuleiro
-isValidPosition :: String -> Bool
-isValidPosition [c, r] = c `elem` ['a' .. 'h'] && r `elem` ['1' .. '8']
+isValidPosition :: (Char, Char) -> Bool
+isValidPosition (c, r) = c `elem` ['a' .. 'h'] && r `elem` ['1' .. '8']
 isValidPosition _ = False
 
--- Converte posição para coordenadas
+
 coordsFromPosition :: String -> (Int, Int)
 coordsFromPosition [c, r] = (fromEnum r - 49, fromEnum c - 97)
+coordsFromPosition _ = error "A posição deve ser uma string de dois caracteres: coluna e linha."
 
 -- Função para verificar se o movimento é válido
 isValidMove :: (Int, Int) -> (Int, Int) -> Board -> Player -> Bool
 isValidMove (x1, y1) (x2, y2) board player =
-  isValidPosition [toEnum (y1 + 97), toEnum (x1 + 49)]
-    && isValidPosition [toEnum (y2 + 97), toEnum (x2 + 49)]
-    && abs (x2 - x1) == 1
-    && abs (y2 - y1) == 1
-    && isEmpty (x2, y2) board -- Movimento diagonal de uma casa
-    && isOwnPiece (x1, y1) board player -- Posição final vazia
-    -- Peça pertence ao jogador atual
+  isValidPosition (c1, c3) && isValidPosition (c2, c4) &&
+  abs (x2 - x1) == 1 && abs (y2 - y1) == 1 &&
+  isEmpty (x2, y2) board &&
+  isOwnPiece (x1, y1) board player
+  where
+    c1 = chr (x1 + 97)
+    c2 = chr (x2 + 97)
+    c3 = chr (y1 + 49)
+    c4 = chr (y2 + 49)
 
 isEmpty :: (Int, Int) -> Board -> Bool
 isEmpty (x, y) board = case board !! x !! y of
@@ -54,7 +59,10 @@ isOwnPiece (x, y) board player = case board !! x !! y of
 readValidPosition :: IO String
 readValidPosition = do
   pos <- getLine
-  if isValidPosition pos
+  let [char, numStr] = words pos
+  let column1 = head char
+  let column2 = head numStr
+  if isValidPosition (column1, column2)
     then return pos
     else do
       putStrLn "Posição inválida. Tente novamente:"
@@ -74,12 +82,24 @@ moveLoop board player = do
   putStrLn $ "Agora selecione o movimento para a peça " ++ startPos ++ "->: "
   endPos <- readValidPosition
   putStrLn $ "Movimento: " ++ startPos ++ " -> " ++ endPos
-  let (x1, y1) = coordsFromPosition startPos
-  let (x2, y2) = coordsFromPosition endPos
+  let [char, numStr] = words startPos
+  let column1 = head char
+  let column2 = head numStr
+  let x1 = fromEnum column1 - 97
+  let y1 = fromEnum column2 - 49
+  print x1
+  print y1
+  let [char, numStr] = words endPos
+  let column1 = head char
+  let column2 = head numStr
+  let x2 = fromEnum column1 - 97
+  let y2 = fromEnum column2 - 49
+  print x2
+  print y2
   if isValidMove (x1, y1) (x2, y2) board player
     then putStrLn "Movimento válido!"
     else do
-      putStrLn "\nMovimento inválido."
+      putStrLn "\nMovimento inválido."  
       putStrLn "\n\n------------------\n\n"
       moveLoop board player
 
